ae6126059a4bee84b0208928a5cdb8d27c8a8f32
sushant kumar <ksushant0293@gmail.com>
2024-07-24 08:29:23 +0530

    gallery page is created

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+
 const Contact = () => {
   return (
     <div>
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Card from '../../components/card/Card'
+import sklfun from "../../assets/sklfun.jpg"
 const Gallery = () => {
   return (
     <div>
       <Header/>
+      <h1 className='bg-green-200 p-3 text-center text-3xl mb-4'>Our Gallery</h1>
+      <Card photo={sklfun}  description="Principal, M.Ed, 20 years of experience in educational administration."  />
     </div>
   )
 }
